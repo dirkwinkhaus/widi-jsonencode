b781cbbfab0accc048fc804949a21b045a0e8e46
Dirk Winkhaus <dirk.winkhaus@check24.de>
2020-02-10 12:46:37 +0100

    add null handling in default strategy

--- demo/performanceConfig.php
+++ demo/performanceConfig.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'text' => "Start performance test\n\n",
     'start' => 1000,
-    'end' => 100000,
+    'end' => 10000,
     'step' => 1000,
 ];
--- src/Strategy/DefaultStrategy.php
+++ src/Strategy/DefaultStrategy.php
@@ -7,6 +7,18 @@ use Widi\JsonEncode\Encoder\Core;
 
 class DefaultStrategy implements StrategyInterface
 {
+    /** @var bool */
+    private $showNullValue;
+
+    /**
+     * DefaultStrategy constructor.
+     * @param bool $showNullValue
+     */
+    public function __construct(bool $showNullValue = true)
+    {
+        $this->showNullValue = $showNullValue;
+    }
+
     public function createStdClass($value, Core $jsonEncoder, array $stack = [])
     {
         $instance = new stdClass();
@@ -14,7 +26,10 @@ class DefaultStrategy implements StrategyInterface
 
         foreach ($methods as $method) {
             $propertyName = $jsonEncoder->getPropertyName($value, $method);
-            $instance->$propertyName = $jsonEncoder->encodeRecursive($value->$method(), $stack);
+            $propertyValue = $jsonEncoder->encodeRecursive($value->$method(), $stack);
+            if (($this->showNullValue === true) || ($propertyValue !== null)) {
+                $instance->$propertyName = $propertyValue;
+            }
         }
 
         return $instance;
