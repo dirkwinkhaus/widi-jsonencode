512db7b16c82301232f7d98887926125bd167376
dirk winkhaus <dirk.winkhaus@check24.de>
2021-03-30 15:24:31 +0200

    to be able to return unencoded

--- src/JsonEncoderInterface.php
+++ src/JsonEncoderInterface.php
@@ -2,7 +2,14 @@
 
 namespace Widi\JsonEncode;
 
+use stdClass;
+
 interface JsonEncoderInterface
 {
     public function encode($value): string;
+
+    /**
+     * @return array|stdClass
+     */
+    public function toStdClass($value);
 }
